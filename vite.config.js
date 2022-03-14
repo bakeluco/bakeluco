5c1820d165b011f42cba888a582c93b410b7d8f9
bakeluco <cassineriolu@gmail.com>
2022-03-14 10:27:52 -0300

    update vite confg

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "www.bakeluco.com.ar",
+  base: "bakeluco.com.ar",
   plugins: [vue()],
 });
